fe8b20c3abd54f5da1d27cb924df5cf6b729020b
Irina Murashka <irvil@tut.by>
2021-04-25 17:10:32 +0100

    api запрос

---
+++ src/js/apiService.js
@@ -0,0 +1,34 @@
+const baseUrl = 'https://pixabay.com/api/';
+
+export default {
+  page: 1,
+  query: '',
+  perPage: 12,
+  key: '21325700-25e62cbcf61651ae4fee03a33',
+
+  fetcArticles: async function () {
+    const requestParams = `?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${keyApi}`;
+
+    try {
+      let response = await fetch(baseUrl + requestParams);
+      let parsedRes = await response.json();
+      this.incrementPage();
+      return parsedRes.hits;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  get searchQuery() {
+    return this.query;
+  },
+  set searchQuery(string) {
+    this.query = string;
+  },
+  incrementPage() {
+    this.page += 1;
+  },
+  resetPage() {
+    this.page = 1;
+  },
+};
